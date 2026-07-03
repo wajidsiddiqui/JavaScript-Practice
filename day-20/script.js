737b91c080243142e38195c4975f7983e773b990
Mohammed Abdul Wajid Siddiqui <wajidsiddiqui49@gmail.com>
2026-07-03 15:45:37 +0530

    Day 20: Added smooth heart animation

--- day-20/script.js
+++ day-20/script.js
@@ -1,15 +1,16 @@
+
 var img = document.querySelector('img')
 var like = document.querySelector('i')
+var likeTimer;
 
 img.addEventListener('dblclick',()=>{
     like.style.opacity = 1;
-    setTimeout(()=>{
+    like.style.transform = "translate(-50%, -50%) scale(1)"
 
+    clearTimeout(likeTimer); 
+
+    likeTimer = setTimeout(()=>{
         like.style.opacity = 0;
+        like.style.transform = "translate(-50%, -50%) scale(0)"
     }, 1000)
-        
-    
-    
-    
-    
 })
